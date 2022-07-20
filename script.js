a7e84f93a672ccf9d6f6120f637276a028a9de22
greenecoke <106470392+greenecoke@users.noreply.github.com>
2022-07-20 11:54:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const change = document.getElementById("pancakes");
+ change.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+
+ const change1 = document.getElementById("vegetable");
+ change1.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change1.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+
+ const change2 = document.getElementById("burger");
+ change2.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change2.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+
+ const change3 = document.getElementById("steak");
+ change3.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change3.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+ const change4 = document.getElementById("sushi");
+ change4.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ 
+ change4.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+ const change5 = document.getElementById("pasta");
+ change5.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change5.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+ const change6 = document.getElementById("bread");
+ change6.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ 
+ change6.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
+ const change7 = document.getElementById("cake");
+ change7.addEventListener('mouseover', (event) =>{
+event.target.style. background="white"
+event.target.style. color="#d1b3c4"
+ })
+ change7.addEventListener('mouseout', (event) =>{
+event.target.style. background="#f7d1cd"
+event.target.style. color="#0b6e4f"
+ })
